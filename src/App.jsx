1ff3df1ab788a466bb56ea1823ce939249059f83
Govind Mali <mali.5@iitj.ac.in>
2024-01-16 01:10:21 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import Task from './components/task';
-import TaskForm from './components/taskform';
+import React, { useState, useEffect } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Task from "./components/task";
+import TaskForm from "./components/taskform";
 import {
   List,
   Typography,
@@ -11,20 +11,20 @@ import {
   Select,
   MenuItem,
   Grid,
-} from '@mui/material';
-import './App.css';
+} from "@mui/material";
+import "./App.css";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
     setTasks(storedTasks);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
   const handleAddTask = (newTask) => {
@@ -51,42 +51,55 @@ const App = () => {
   };
 
   const filteredTasks = tasks.filter((task) => {
-    if (filter === 'all') return true;
-    if (filter === 'completed') return task.completed;
-    if (filter === 'incomplete') return !task.completed;
+    if (filter === "all") return true;
+    if (filter === "completed") return task.completed;
+    if (filter === "incomplete") return !task.completed;
     return true;
   });
 
   return (
-    <div>
-      <Typography variant="h2" align="center" style={{ marginTop: '5px'  }}>
-        Task Tracker
-      </Typography>
-      <Grid container direction="row" alignItems="center">
-        <Grid item container justifyContent="center" alignItems="center"  direction="row">
-          <FormControl>
-            <InputLabel>Filter:</InputLabel>
-            <Select
-              value={filter}
-              onChange={(e) => setFilter(e.target.value)}
-              label="Filter"
-            >
-              <MenuItem value="all">All</MenuItem>
-              <MenuItem value="completed">Completed</MenuItem>
-              <MenuItem value="incomplete">Incomplete</MenuItem>
-            </Select>
-          </FormControl>
+    <div display="flex" justifyContent="space-between">
+      <div></div>
+      <div>
+        <Typography variant="h2" align="center" style={{ marginTop: "5px" }}>
+          Task Tracker
+        </Typography>
+        <Grid container direction="row" alignItems="center">
+          <Grid
+            item
+            container
+            justifyContent="end"
+            alignItems="center"
+            direction="row"
+          >
+            <FormControl>
+              <InputLabel>Filter:</InputLabel>
+              <Select
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+                label="Filter"
+              >
+                <MenuItem value="all">All</MenuItem>
+                <MenuItem value="completed">Completed</MenuItem>
+                <MenuItem value="incomplete">Incomplete</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <DndProvider backend={HTML5Backend}>
+            <Grid container direction="column" alignItems="center">
+              <TaskForm onAddTask={handleAddTask} />
+              <Grid>
+                <TaskList
+                  tasks={filteredTasks}
+                  onDelete={handleDeleteTask}
+                  onToggleComplete={handleToggleComplete}
+                  moveTask={handleMoveTask}
+                />
+              </Grid>
+            </Grid>
+          </DndProvider>
         </Grid>
-        <DndProvider backend={HTML5Backend}>
-          <TaskForm onAddTask={handleAddTask} />
-          <TaskList
-            tasks={filteredTasks}
-            onDelete={handleDeleteTask}
-            onToggleComplete={handleToggleComplete}
-            moveTask={handleMoveTask}
-          />
-        </DndProvider>
-      </Grid>
+      </div>
     </div>
   );
 };
